1691c14e2a545794bf54f7655b2dbdd49860dc09
Daniel Spósito Coelho <85141838+danielspositocoelho@users.noreply.github.com>
2021-11-22 11:50:34 -0300

    Dia de entrega
    faltou o ultimo projeto

--- Matrizes/Gabarito/Program.cs
+++ Matrizes/Gabarito/Program.cs
@@ -10,12 +10,104 @@ namespace Gabarito
     {
         static void Main(string[] args)
         {
- /*Declare um array de char com 10 posições onde será armazenado o gabarito da prova (de a até e). Declare uma matriz 100 x 10 que se refere às respostas de 100 alunos.
-* Preencha a matriz com números aleatórios de 1 a 5, sendo que 1 corresponde ao “a” e 5 ao “e” .
-* Comparar as respostas de cada candidato com o gabarito e armazenar um array de inteiros chamado resultado, contendo a pontuação correspondente a cada aluno.
-* Para cada acerto some um ponto. Exiba o gabarito, a matriz e a pontuação.*/
+            /*Declare um array de char com 10 posições onde será armazenado o gabarito da prova (de a até e). Declare uma matriz 100 x 10 que se refere às respostas de 100 alunos.
+           * Preencha a matriz com números aleatórios de 1 a 5, sendo que 1 corresponde ao “a” e 5 ao “e” .
+           * Comparar as respostas de cada candidato com o gabarito e armazenar um array de inteiros chamado resultado, contendo a pontuação correspondente a cada aluno.
+           * Para cada acerto some um ponto. Exiba o gabarito, a matriz e a pontuação.*/
 
+            // char array gabarito 10
+            char[] gabarito = new char[] { 'a', 'e', 'c', 'b', 'd', 'c', 'e', 'a', 'a', 'b' };
+            Console.WriteLine("Este é o gabarito:\n");
+            for (int x = 0; x < gabarito.Length; x++)
+            {
+                Console.Write(gabarito[x]+" ");
+            }
 
+            Console.WriteLine("\n\nEstas são as respostas dados por 100 alunos");
+            //random int jagged array respostas 100 x 10 -números aleatórios de 1 a 5, sendo que 1 corresponde ao “a” e 5 ao “e”
+            Random gerador = new Random();
+            int[,] respostas = new int[100,10];
+            for(int i=0; i<respostas.GetLength(0); i++)
+            {
+                for (int y=0; y<respostas.GetLength(1); y++)
+                {
+                    respostas[i, y] = gerador.Next(1, 5);
+                    Console.Write(respostas[i,y]);
+                }
+                Console.WriteLine("");
+            }
+            //comparando linha de respostas com gabarito e armazenando pontuação em int array resultado
+            int[] resultado = new int[10];
+            for (int i = 0; i < respostas.GetLength(0); i++)
+            {
+                int pontos = 0;
+                Console.Write("A pontuação do aluno {0} foi ", i+1);
+                for (int y = 0; y < respostas.GetLength(1); y++)
+                {   
+                    switch (respostas[i,y])
+                    {
+                        case 1:
+                            if (gabarito[y] == 'a')
+                            {
+                                resultado[y] = 1;
+                            }
+                            else
+                            {
+                                resultado[y] = 0;
+                            }
+                                break;
+
+                        case 2:
+                            if (gabarito[y] == 'b')
+                            {
+                                resultado[y] = 1;
+                            }
+                            else
+                            {
+                                resultado[y] = 0;
+                            }
+                            break;
+
+                        case 3:
+                            if (gabarito[y] == 'c')
+                            {
+                                resultado[y] = 1;
+                            }
+                            else
+                            {
+                                resultado[y] = 0;
+                            }
+                            break;
+
+                        case 4:
+                            if (gabarito[y] == 'd')
+                            {
+                                resultado[y] = 1;
+                            }
+                            else
+                            {
+                                resultado[y] = 0;
+                            }
+                            break;
+
+                        case 5:
+                            if (gabarito[y] == 'e')
+                            {
+                                resultado[y] = 1;
+                            }
+                            else
+                            {
+                                resultado[y] = 0;
+                            }
+                            break;
+                    }
+                    if (resultado[y] == 1)
+                        pontos++;
+                    Console.Write(resultado[y]+" ");
+                }
+                Console.Write(": Total de {0} pontos\n", pontos);
+            }
+            Console.ReadLine();
         }
     }
 }
--- Matrizes/JogoDaVelha/Program.cs
+++ Matrizes/JogoDaVelha/Program.cs
@@ -12,6 +12,60 @@ namespace JogoDaVelha
         {
             /*Jogo da velha: Crie um tabuleiro de jogo da velha, em uma matriz de char de 3x3 e preencha com 0 e X alternadamente, começando sempre por 0.
              * O valor deve ser preenchido no local indicado pelo usuário, o usuário precisa indicar o local informando a linha e a coluna. Por exemplo linha 2, coluna 2.*/
+            char continuar = 's';
+            char[][] tabuleiro = new char[3][] { new char[3], new char[3], new char[3] };
+            do
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    for (int y = 0; y < 3; y++)
+                    {
+                        Console.Write("| | ");
+                    }
+                    Console.WriteLine("");
+                }
+
+                Console.WriteLine("Escolha o lugar em que deseja colocar o 0 no tabuleiro:");
+                for (int jogo=0; jogo<9; jogo++)
+                {
+                    Console.WriteLine("Informe a linha:");
+                    int linha = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Informe a coluna:");
+                    int coluna = int.Parse(Console.ReadLine());
+
+                    for (int i = 0; i < tabuleiro[0].Length; i++)
+                    {
+                        for (int z = 0; z < tabuleiro[1].Length; z++)
+                        {
+                            if (i==linha-1 && z==coluna-1 && tabuleiro[i][z] != '0')
+                            {
+                                tabuleiro[i][z] = '0';
+                                if (z != 2)
+                                {
+                                    tabuleiro[i][z + 1] = 'X';
+                                }
+                                else if (z == 2 && i != 2)
+                                {
+                                    tabuleiro[i + 1][z] = 'X';
+                                }
+                                else
+                                    break;
+                            }
+                            else if(i == linha - 1 && z == coluna - 1 && tabuleiro[i][z] == '0')
+                            {
+                                Console.WriteLine("Escolha outra casa, esta ja foi escolhida");
+                            }
+                            else
+                            {
+                                tabuleiro[i][z] = ' ';
+                            }
+                            Console.Write("|" + tabuleiro[i][z] + "| ");
+                        }
+                        Console.WriteLine("");
+                    }
+                }
+                Console.WriteLine("Deseja continuar? Digite 's' para repetir e qualquer outra coisa para sair" );
+            } while (continuar == 's');
 
         }
     }
