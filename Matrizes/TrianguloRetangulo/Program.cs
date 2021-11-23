47b398f746dabf8f86e9a3d17e9729d8857ee827
Daniel Spósito Coelho <spositocoelho@gmail.com>
2021-11-23 20:17:18 -0300

    Fixing Bug with jagged array
    Tem como fazer com multidimensional?

--- Matrizes/TrianguloRetangulo/Program.cs
+++ Matrizes/TrianguloRetangulo/Program.cs
@@ -10,34 +10,36 @@ namespace TrianguloRetangulo
     {
         static void Main(string[] args)
         {
-            /*TrianguloRetangulo
-            Declare uma matriz de char de 6 x 6, e desenhe um triangulo retângulo conforme exemplo: Exiba a matriz*/
-
-            //HÁ UM BUG AQUI, PRECISA ARRUMAR
-            char continuar = 's';
+           char continuar = 's';
             do
-            {               
-                    Console.WriteLine("Qual é a largura do triangulo retângulo desejado?");
-                    int largura = int.Parse(Console.ReadLine());
+            {
+                Console.WriteLine("Qual é a largura do triangulo retângulo desejado?");
+                int largura = int.Parse(Console.ReadLine());
 
-                    Console.WriteLine("Qual é a altura da triangulo retângulo desejado?");
-                    int altura = int.Parse(Console.ReadLine());
+                Console.WriteLine("Qual é a altura da triangulo retângulo desejado?");
+                int altura = int.Parse(Console.ReadLine());
 
-                char[,] piramide = new char[altura, largura];
-                for (int i=0; i<piramide.GetLength(0); i++)
+                char[][] piramide = new char[altura][];
+                for (int a=0; a<altura;a++)
                 {
-                    for (int y = 0; y < piramide.GetLength(1); y++)
+                    piramide[a] = new char[largura];
+                }
+
+                piramide[0] = new char[largura];
+                for (int i = 0; i < piramide.Length; i++)
+                {
+                    for (int y = 0; y < piramide[0].Length; y++)
                     {
                         if (y <= i)
-                            piramide[i, y] = '*';
+                            piramide[i][y] = '*';
                     }
                 }
 
-                for (int i = 0; i < piramide.GetLength(0); i++)
+                for (int i = 0; i < piramide.Length; i++)
                 {
-                    for (int y = 0; y < piramide.GetLength(1); y++)
+                    for (int y = 0; y < piramide[0].Length; y++)
                     {
-                        Console.Write(piramide[i, y]);
+                        Console.Write(piramide[i][y]);
                     }
                     Console.WriteLine("");
                 }
