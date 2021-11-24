f39b91b5ecfb7e5d6193ba45c1001fca7d881195
Daniel Spósito Coelho <85141838+danielspositocoelho@users.noreply.github.com>
2021-11-23 22:44:26 -0300

    Ultimo Exercicio feito - quase pronto
    falta corrigir loop que controla as jogadas e impedir jogador de sobreescrever

--- Matrizes/JogoDaVelha/Program.cs
+++ Matrizes/JogoDaVelha/Program.cs
@@ -10,10 +10,9 @@ namespace JogoDaVelha
     {
         static void Main(string[] args)
         {
-            /*Jogo da velha: Crie um tabuleiro de jogo da velha, em uma matriz de char de 3x3 e preencha com 0 e X alternadamente, começando sempre por 0.
-             * O valor deve ser preenchido no local indicado pelo usuário, o usuário precisa indicar o local informando a linha e a coluna. Por exemplo linha 2, coluna 2.*/
             char continuar = 's';
-            char[][] tabuleiro = new char[3][] { new char[3], new char[3], new char[3] };
+            char[,] tabuleiro = new char[3, 3];
+
             do
             {
                 for (int x = 0; x < 3; x++)
@@ -25,46 +24,56 @@ namespace JogoDaVelha
                     Console.WriteLine("");
                 }
 
-                Console.WriteLine("Escolha o lugar em que deseja colocar o 0 no tabuleiro:");
-                for (int jogo=0; jogo<9; jogo++)
+                bool continua = true;
+                Console.WriteLine("Escolha o lugar em que deseja colocar o 0 no tabuleiro. Não é permitido colocar peças em lugares já ocupados. Seja legal :-)");
+                while (continua == true)
                 {
-                    Console.WriteLine("Informe a linha:");
+                    continua = false;
+                    Console.WriteLine("Jogador 1 [O] - Informe a linha:");
                     int linha = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Informe a coluna:");
+                    Console.WriteLine("Jogador 1 [O] - Informe a coluna:");
                     int coluna = int.Parse(Console.ReadLine());
 
-                    for (int i = 0; i < tabuleiro[0].Length; i++)
+                    for (int i = 0; i < tabuleiro.GetLength(0); i++)
                     {
-                        for (int z = 0; z < tabuleiro[1].Length; z++)
+                        for (int z = 0; z < tabuleiro.GetLength(1); z++)
                         {
-                            if (i==linha-1 && z==coluna-1 && tabuleiro[i][z] != '0')
-                            {
-                                tabuleiro[i][z] = '0';
-                                if (z != 2)
-                                {
-                                    tabuleiro[i][z + 1] = 'X';
-                                }
-                                else if (z == 2 && i != 2)
-                                {
-                                    tabuleiro[i + 1][z] = 'X';
-                                }
-                                else
-                                    break;
-                            }
-                            else if(i == linha - 1 && z == coluna - 1 && tabuleiro[i][z] == '0')
-                            {
-                                Console.WriteLine("Escolha outra casa, esta ja foi escolhida");
-                            }
-                            else
-                            {
-                                tabuleiro[i][z] = ' ';
-                            }
-                            Console.Write("|" + tabuleiro[i][z] + "| ");
+                            if (i == linha - 1 && z == coluna - 1)
+                                tabuleiro[i, z] = 'O';
+
+                            Console.Write("|" + tabuleiro[i, z] + "| ");
+                        }
+                        Console.WriteLine("");
+                    }
+
+                    Console.WriteLine("Jogador 2 [X] - Informe a linha:");
+                    linha = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Jogador 2 [X] - Informe a coluna:");
+                    coluna = int.Parse(Console.ReadLine());
+
+                    for (int i = 0; i < tabuleiro.GetLength(0); i++)
+                    {
+                        for (int z = 0; z < tabuleiro.GetLength(1); z++)
+                        {
+                            if (i == linha - 1 && z == coluna - 1)
+                                tabuleiro[i, z] = 'X';
+
+                            Console.Write("|" + tabuleiro[i, z] + "| ");
+                        }
+                        Console.WriteLine("");
+                    }
+
+                    for (int i = 0; i < tabuleiro.GetLength(0); i++)
+                    {
+                        for (int z = 0; z < tabuleiro.GetLength(1); z++)
+                        {
+                            if (tabuleiro[i, z] != 'O' && tabuleiro[i, z] != 'X')
+                                continua = true;
                         }
                         Console.WriteLine("");
                     }
                 }
-                Console.WriteLine("Deseja continuar? Digite 's' para repetir e qualquer outra coisa para sair" );
+                Console.WriteLine("Deseja continuar? Digite 's' para repetir e qualquer outra coisa para sair");
             } while (continuar == 's');
 
         }
